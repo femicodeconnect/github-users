6bf970aed44d48923595dbd5cc55d9b4c3f7190b
Femi <femiconnect234@gmail.com>
2021-05-16 18:16:29 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Dashboard, Login, Error } from './pages';
+
+function App() {
+   return (
+      <div>
+         <Dashboard />
+         <Login />
+         <Error />
+      </div>
+   );
+}
+
+export default App;
---
+++ src/context/context.js
@@ -0,0 +1,2 @@
+import React from 'react';
+const rootUrl = 'https://api.github.com';
