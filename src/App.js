8b51192494b8c87576ffab5fb01042e299c9036c
Femi <femiconnect234@gmail.com>
2021-05-16 18:38:08 +0100

    setup routing in the App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Dashboard, Login, Error } from './pages';
+//The line of code above imports index.js file in the pages folder. All the files imports was done in the index.js file
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
    return (
-      <div>
-         <Dashboard />
-         <Login />
-         <Error />
-      </div>
+      <Router>
+         <Switch>
+            <Route path='/' exact={true}>
+               <Dashboard></Dashboard>
+            </Route>
+            <Route path='/login'>
+               <Login></Login>
+            </Route>
+            <Route path='*'>
+               <Error></Error>
+            </Route>
+         </Switch>
+      </Router>
    );
 }
 
