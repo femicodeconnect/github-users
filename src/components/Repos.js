7eb907b7ae607eb96231d7eaeb80d813509502f7
Femi <femiconnect234@gmail.com>
2021-05-17 15:56:00 +0100

    rebuilt a pie chart with global state data from Repos component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -7,6 +7,48 @@ const Repos = () => {
    //destruture repos array from global state
    const { repos } = useContext(GithubContext);
 
+   //use the reduce function to get the languages & the total occurence of a particular language in the array
+   let languages = repos.reduce((total, item) => {
+      //get the language property for each repo in repos
+      const { language } = item;
+
+      //if language is null, move to next iteration
+      if (!language) return total;
+
+      console.log(language);
+
+      //for first occurence of a particular language
+      if (!total[language]) {
+         //total[language] = 1;
+
+         //format into a form usable by the chart
+         total[language] = { label: language, value: 1 };
+      } else {
+         //for another occurence of a particular lang
+         //total[language] = total[language] + 1;
+
+         //format into a form usable by the chart
+         total[language] = {
+            ...total[language],
+            value: total[language].value + 1,
+         };
+      }
+      return total;
+   }, {});
+
+   console.log(languages);
+
+   //The following will be done on the languages object by chaining
+   //1. turning languages object into an array
+   //2. sorting the array of languages in order of popularity
+   //3. slicing the array to get the first five items in it (if the array has more than 5 elements)
+   languages = Object.values(languages)
+      .sort((a, b) => {
+         return b.value - a.value;
+      })
+      .slice(0, 5);
+   console.log(languages);
+
    const chartData = [
       {
          label: 'HTML',
@@ -22,10 +64,11 @@ const Repos = () => {
       },
    ];
 
+   //passing the languages array as data to be used in the chart
    return (
       <section className='section'>
          <Wrapper className='section-center'>
-            <Pie3D data={chartData} />
+            <Pie3D data={languages} />
             {/* <ExampleChart data={chartData} />; */}
          </Wrapper>
       </section>
@@ -43,6 +86,8 @@ const Wrapper = styled.div`
    @media (min-width: 1200px) {
       grid-template-columns: 2fr 3fr;
    }
+
+   //the code below makes the chart container responsive
 
    div {
       width: 100% !important;
