c93c03ac63d559394ec95b192e80ba502d098473
Femi <femiconnect234@gmail.com>
2021-05-19 22:31:43 +0100

    setup imported context for authentication from auth0 library

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+//  domain - dev-1tpiw-zp.us.auth0.com
+//clientID - FYiCAg45539wKU4xvHmSOSjxTjFaYT32
+//redirectUri - {window.location.origin}
 
 ReactDOM.render(
    <React.StrictMode>
-      <GithubProvider>
-         <App />
-      </GithubProvider>
+      <Auth0Provider
+         domain='dev-1tpiw-zp.us.auth0.com'
+         clientId='FYiCAg45539wKU4xvHmSOSjxTjFaYT32'
+         redirectUri={window.location.origin}
+      >
+         <GithubProvider>
+            <App />
+         </GithubProvider>
+      </Auth0Provider>
    </React.StrictMode>,
    document.getElementById('root')
 );
